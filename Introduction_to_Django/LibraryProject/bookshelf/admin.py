dafddfe6fd0e42d1fb94afd1482008449a6d4589
dina <dinamah3030@gmail.com>
2025-12-06 14:07:39 +0200

    Delete Introduction_to_Django/LibraryProject directory

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # عرض الأعمدة
-    list_filter = ('publication_year', 'author')            # فلترة حسب السنة أو المؤلف
-    search_fields = ('title', 'author')                     # البحث داخل العنوان والمؤلف
-
-admin.site.register(Book, BookAdmin)
