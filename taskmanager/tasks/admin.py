3c7f5b8d3dec1f7692ebf30ad382edb4713878ce
dina <dinamah3030@gmail.com>
2025-12-20 16:15:50 +0200

    Delete taskmanager directory

--- taskmanager/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'period', 'user')   # The columns that appear in the administration panel
-    search_fields = ('title', 'description')     #Searching inside the oryx
-    list_filter = ('period', 'user')             # Filters for period and user
