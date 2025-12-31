e9a2d6ace91831a28da5ced3a6daf2644613604c
dina <dinamah3030@gmail.com>
2025-12-31 16:29:48 +0200

    Delete task_manager directory

--- task_manager/tasks/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'period', 'due_date', 'user')
-    list_filter = ('status', 'period', 'due_date', 'user')
-    search_fields = ('title', 'description')
-    ordering = ('status', 'due_date')
